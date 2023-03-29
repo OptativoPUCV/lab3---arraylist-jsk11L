63fa3f03b5621e51b6ee2a42ee80c18b4af52d26
Replit user <>
2023-03-29 03:22:40 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -32,28 +32,34 @@ void append(ArrayList * l, void * data){
 
 void push(ArrayList * l, void * data, int i){
     if(i > l->size) return;
+  
     if (l->size == l->capacity){
         l->capacity *= 2;
         l->data = (void *) realloc (l->data, l->capacity*sizeof(void));
     }
+  
     for(int j = l->size; j > i; j--){
       l->data[j] = l->data[j-1];
     }
+  
     l->data[i] = data;
     l->size++;
-
-}
-
-void* pop(ArrayList * l, int i){
-    return NULL;
+    return;
 }
 
 void* get(ArrayList * l, int i){
-    return NULL;
+    if (i >= l->size) return NULL;
+    if (i < 0) i += l->size+1;
+    
+    return l->data[i];
 }
 
 int get_size(ArrayList * l){
     return l->size;
+}
+
+void* pop(ArrayList * l, int i){
+    return;
 }
 
 //remove elements
